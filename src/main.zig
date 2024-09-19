77fd86e8402ade3889c26a17063fd1b1e97624e6
Luke Mattfeld <lukemattfeld@gmail.com>
2024-09-18 20:10:40 -0700

    renamed lib import

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 const math = std.math;
-const r = @cImport({
+const raylib = @cImport({
     @cInclude("raylib.h");
 });
 
@@ -8,30 +8,30 @@ pub fn main() !void {
     const screenWidth = 800;
     const screenHeight = 450;
 
-    r.InitWindow(screenWidth, screenHeight, "RayZig Window :)");
-    r.SetTargetFPS(120);
-    defer r.CloseWindow();
+    raylib.InitWindow(screenWidth, screenHeight, "RayZig Window :)");
+    raylib.SetTargetFPS(120);
+    defer raylib.CloseWindow();
 
-    var ballPos = r.Vector2{ .x = @as(f32, screenWidth) / 2, .y = @as(f32, screenHeight) / 2 };
+    var ballPos = raylib.Vector2{ .x = @as(f32, screenWidth) / 2, .y = @as(f32, screenHeight) / 2 };
     var ballRad: f32 = 10.0;
 
-    while (!r.WindowShouldClose()) {
-        if (r.IsKeyDown(r.KEY_RIGHT)) {
+    while (!raylib.WindowShouldClose()) {
+        if (raylib.IsKeyDown(raylib.KEY_RIGHT)) {
             ballPos.x += 2.0;
         }
-        if (r.IsKeyDown(r.KEY_LEFT)) {
+        if (raylib.IsKeyDown(raylib.KEY_LEFT)) {
             ballPos.x -= 2.0;
         }
-        if (r.IsKeyDown(r.KEY_UP)) {
+        if (raylib.IsKeyDown(raylib.KEY_UP)) {
             ballPos.y -= 2.0;
         }
-        if (r.IsKeyDown(r.KEY_DOWN)) {
+        if (raylib.IsKeyDown(raylib.KEY_DOWN)) {
             ballPos.y += 2.0;
         }
-        if (r.IsKeyDown(r.KEY_P)) {
+        if (raylib.IsKeyDown(raylib.KEY_P)) {
             ballRad += 2.0;
         }
-        if (r.IsKeyDown(r.KEY_O)) {
+        if (raylib.IsKeyDown(raylib.KEY_O)) {
             ballRad -= 2.0;
         }
 
@@ -39,10 +39,10 @@ pub fn main() !void {
         ballPos.x = math.clamp(ballPos.x, ballRad, screenWidth - ballRad);
         ballPos.y = math.clamp(ballPos.y, ballRad, screenHeight - ballRad);
 
-        r.BeginDrawing();
-        r.ClearBackground(r.BLACK);
-        r.DrawText("Hello Window from Zig!", 10, screenWidth / 2, 20, r.LIGHTGRAY);
-        r.DrawCircleV(ballPos, ballRad, r.MAROON);
-        r.EndDrawing();
+        raylib.BeginDrawing();
+        raylib.ClearBackground(raylib.BLACK);
+        raylib.DrawText("Hello Window from Zig!", 10, screenWidth / 2, 20, raylib.LIGHTGRAY);
+        raylib.DrawCircleV(ballPos, ballRad, raylib.MAROON);
+        raylib.EndDrawing();
     }
 }
