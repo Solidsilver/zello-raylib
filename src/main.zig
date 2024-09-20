29628b683da2cb48de079119a9f3a9bb60e75a0c
Luke Mattfeld <lukemattfeld@gmail.com>
2024-09-20 13:21:04 -0700

    added paddles, fps text

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const math = std.math;
+const paddle = @import("paddle.zig");
 const raylib = @cImport({
     @cInclude("raylib.h");
 });
@@ -12,37 +13,54 @@ pub fn main() !void {
     raylib.SetTargetFPS(120);
     defer raylib.CloseWindow();
 
-    var ballPos = raylib.Vector2{ .x = @as(f32, screenWidth) / 2, .y = @as(f32, screenHeight) / 2 };
+    var p1Pos = raylib.Vector2{ .x = @as(f32, screenWidth) / 2, .y = @as(f32, screenHeight) / 2 };
     var ballRad: f32 = 10.0;
+    var p1 = paddle.New(paddle.ScreenSide.left, 10, screenWidth, screenHeight);
+    var p2 = paddle.New(paddle.ScreenSide.right, 10, screenWidth, screenHeight);
+
+    var fps: i32 = 0;
+    // var fpsText := std.fmt.allocPrint(allocator: allocator, "{} fps", .{fps})
+    var fps_text_buffer: [20]u8 = undefined; // Allocate enough space
 
     while (!raylib.WindowShouldClose()) {
-        if (raylib.IsKeyDown(raylib.KEY_RIGHT)) {
-            ballPos.x += 2.0;
-        }
-        if (raylib.IsKeyDown(raylib.KEY_LEFT)) {
-            ballPos.x -= 2.0;
-        }
+        fps = raylib.GetFPS();
+        const fps_text = std.fmt.bufPrint(&fps_text_buffer, "{d} fps", .{fps}) catch unreachable;
+        fps_text_buffer[fps_text.len] = 0;
+
         if (raylib.IsKeyDown(raylib.KEY_UP)) {
-            ballPos.y -= 2.0;
+            p1.Move(-2);
         }
         if (raylib.IsKeyDown(raylib.KEY_DOWN)) {
-            ballPos.y += 2.0;
+            p1.Move(2);
         }
-        if (raylib.IsKeyDown(raylib.KEY_P)) {
-            ballRad += 2.0;
-        }
-        if (raylib.IsKeyDown(raylib.KEY_O)) {
-            ballRad -= 2.0;
-        }
+        // const keyDown = raylib.GetKeyPressed();
+        // switch (keyDown) {
+        //     raylib.KEY_UP => {
+        //         p1.Move(-2);
+        //     },
+        //     raylib.KEY_DOWN => {
+        //         p1.Move(2);
+        //     },
+        //     else => {},
+        // }
+        // if (raylib.IsKeyDown(raylib.KEY_P)) {
+        //     ballRad += 2.0;
+        // }
+        // if (raylib.IsKeyDown(raylib.KEY_O)) {
+        //     ballRad -= 2.0;
+        // }
 
         ballRad = math.clamp(ballRad, 4, @min(screenWidth, screenHeight) / 2);
-        ballPos.x = math.clamp(ballPos.x, ballRad, screenWidth - ballRad);
-        ballPos.y = math.clamp(ballPos.y, ballRad, screenHeight - ballRad);
+        p1Pos.x = math.clamp(p1Pos.x, ballRad, screenWidth - ballRad);
+        p1Pos.y = math.clamp(p1Pos.y, ballRad, screenHeight - ballRad);
 
         raylib.BeginDrawing();
         raylib.ClearBackground(raylib.BLACK);
-        raylib.DrawText("Hello Window from Zig!", 10, screenWidth / 2, 20, raylib.LIGHTGRAY);
-        raylib.DrawCircleV(ballPos, ballRad, raylib.MAROON);
+        const fpsTextLen: i32 = @intCast(fps_text.len);
+        raylib.DrawText(fps_text.ptr, (screenWidth / 2) - (fpsTextLen * 6), 2, 20, raylib.LIGHTGRAY);
+        raylib.DrawCircleV(p1Pos, ballRad, raylib.MAROON);
+        p1.Draw();
+        p2.Draw();
         raylib.EndDrawing();
     }
 }
---
+++ src/paddle.zig
@@ -0,0 +1,36 @@
+const std = @import("std");
+const math = std.math;
+const raylib = @cImport({
+    @cInclude("raylib.h");
+});
+
+pub const ScreenSide = enum { left, right };
+
+pub const Paddle = struct {
+    pos: raylib.Vector2,
+    size: raylib.Vector2,
+    color: raylib.Color,
+    screenBounds: raylib.Vector2,
+
+    pub fn Draw(self: *Paddle) void {
+        raylib.DrawRectangleV(self.pos, self.size, self.color);
+    }
+
+    pub fn Move(self: *Paddle, dY: f32) void {
+        self.pos.y += dY;
+        self.pos.y = math.clamp(self.pos.y, 0, self.screenBounds.y - self.size.y);
+    }
+};
+
+pub fn New(side: ScreenSide, size: comptime_int, comptime windowX: i32, comptime windowY: i32) Paddle {
+    var drawPosX: f32 = 0.0;
+    const rect = raylib.Vector2{ .x = size * 2, .y = size * 10 };
+    const bounds = raylib.Vector2{
+        .x = @as(f32, windowX),
+        .y = @as(f32, windowY),
+    };
+    if (side == ScreenSide.right) {
+        drawPosX = bounds.x - rect.x;
+    }
+    return Paddle{ .pos = raylib.Vector2{ .x = drawPosX, .y = bounds.y / 2 }, .size = rect, .color = raylib.WHITE, .screenBounds = bounds };
+}
